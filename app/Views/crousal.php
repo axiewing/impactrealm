8257cb1b3a2d1c3d337e40c189bdf7c302c84c61
Mehmood7 <mjhassan321@gmail.com>
2022-11-05 22:36:45 +0500

    dashboard v1.2

---
+++ app/Views/crousal.php
@@ -0,0 +1,11 @@
+<div class="owl-carousel testimonial-carousel">
+    <div class="testimonial-item text-center">
+        <img class="" src="img/hdr1.jpg" style="height: 500px;">
+    </div>
+    <div class="testimonial-item text-center">
+        <img class="" src="img/hdr2.jpg" style="height: 500px;">
+    </div>
+    <div class="testimonial-item text-center">
+        <img class="" src="img/hdr3.jpg" style="height: 500px;">
+    </div>
+</div>
--- app/Views/landing.php
+++ app/Views/landing.php
@@ -1,13 +1,64 @@
 <?php
 
+use Predis\Command\Redis\APPEND;
+
 include('top.php');
+include('crousal.php');
 ?>
 
-<div class="p-3">
-            <h3>Landing Page</h3>
-            <p>Coming soon</p></div>
-<?php
 
+<?php
+$events = array();
+array_push($events,
+(object)[
+    'title'=>'Bitcoin event to dicuss the future',
+    'time' => 'Fri, Dec 2, 9:30 PM',
+    'cost' => 'Free',
+    'address'=>'Harverd university, main block auditorium',
+    'img_name' => 'imge1.jpg'
+],
+(object)[
+    'title'=>'Bitcoin event to dicuss the future',
+    'time' => 'Fri, Dec 2, 9:30 PM',
+    'cost' => 'Free',
+    'address'=>'Harverd university, main block auditorium',
+    'img_name' => 'imge2.jpg'
+],
+(object)[
+    'title'=>'Bitcoin event to dicuss the future',
+    'time' => 'Fri, Dec 2, 9:30 PM',
+    'cost' => 'Free',
+    'address'=>'Harverd university, main block auditorium',
+    'img_name' => 'imge3.jpg'
+],
+(object)[
+    'title'=>'Bitcoin event to dicuss the future',
+    'time' => 'Fri, Dec 2, 9:30 PM',
+    'cost' => 'Free',
+    'address'=>'Harverd university, main block auditorium',
+    'img_name' => 'imge4.jpg'
+],
+(object)[
+    'title'=>'Bitcoin event to dicuss the future',
+    'time' => 'Fri, Dec 2, 9:30 PM',
+    'cost' => 'Free',
+    'address'=>'Harverd university, main block auditorium',
+    'img_name' => 'imge5.jpg'
+]
+);
+$events_obj = (object)[
+    "title" => "Latest Events",
+    "events" => $events
+];
+
+include ('event_listing.php');
+array_pop($events);
+$events_obj = (object)[
+    "title" => "Popular Events",
+    "events" => $events
+];
+
+include ('event_listing.php');
 
 include('bottom.php');
 
