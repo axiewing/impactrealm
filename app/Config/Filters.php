678ca49e3bd466f591603d2afab1d79da69ca306
Mehmood7 <mjhassan321@gmail.com>
2022-12-21 15:27:46 +0500

    show landing page and about without login.

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -36,7 +36,7 @@ class Filters extends BaseConfig
             // 'honeypot',
              'csrf',
             // 'invalidchars',
-            'session' => ['except' => ['login*', 'register', 'auth/a/*', 'oauth*']],
+            'session' => ['except' => ['login*', 'register', 'auth/a/*', 'oauth*','/','about']],
         ],
         'after' => [
             'toolbar',
